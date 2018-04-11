a847264694956b96bc1dda868166f445c661fe9f
Claire Wu <381506245@qq.com>
2018-04-11 10:14:16 +0100

    Earth spinning in Milky Way

---
+++ Assets/Spin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+    public float Degrees;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.down * Time.deltaTime * Degrees);
+	}
+}
