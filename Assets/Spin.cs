d15b24661d9a90c519b04e58ab7b223036c4753b
Claire Wu <381506245@qq.com>
2018-04-11 14:30:39 +0100

    Added click event to Earth globe and pointer raycaster

--- Assets/Spin.cs
+++ Assets/Spin.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spin : MonoBehaviour {
-
-    public float Degrees;
+public class Spin : MonoBehaviour
+{
+    public float degreesPerSecond = 20;
 
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(Vector3.down * Time.deltaTime * Degrees);
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.down * Time.deltaTime * this.degreesPerSecond);
+    }
+
+    public void flipSpin()
+    {
+        this.degreesPerSecond = -this.degreesPerSecond;
+    }
 }
